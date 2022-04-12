4e7e2132f80d2d6aba2a72ed6f5a64ee983eec21
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-04-12 16:01:39 -0400

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Grid, Container, Form, Header, Message } from 'semantic-ui-react'
+import { faucetRequest } from './api';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // Component State Handler
+    /**
+     * @property {String} address - Address to make API request with
+     * @property {String} error - Error that ocurred from funding API call if any
+     * @property {Boolean} loading - Is the API Call in progress?
+     * @property {Boolean} success - Was the API Call a success? ( No errors in res.data, etc ) 
+     */
+    let [componentState, setComponentState] = React.useState({
+        address: "",
+        error: "",
+        loading: "",
+        success: ""
+    });
+    const updateComponentState = (stateUpdates) => setComponentState(s => ({ ...s, ...stateUpdates }));
+
+    /**
+     * Submit an api request for funding with current local address state
+     */
+    const submitFaucetRequest = async () => {
+        if (!componentState.address) {
+            return updateComponentState({ error: "An address is required" })
+        }
+        updateComponentState({ loading: true, success: "", error: "" });
+        let apiRes = await faucetRequest(componentState.address);
+        if (apiRes.error) {
+            updateComponentState({ error: apiRes.error, success: false });
+        } else {
+            updateComponentState({ error: false, success: true });
+        }
+        updateComponentState({ loading: false });
+    }
+
+    const ResultMessage = () => componentState.success || componentState.error ? componentState.success ? (
+        <Message success>
+            Funding request successful for {componentState.address}
+        </Message>
+    ) : (
+        <Message error>
+            {componentState.error.toLocaleUpperCase()}
+        </Message>
+    ) : null;
+
+    return (
+        <Grid columns="1">
+            <Grid.Column>
+                <Container className="mt-20">
+
+                    <Header content="AliceNet Faucet" subheader="For testnet located at: https://mad.catmad.duckdns.org" />
+
+                    <Form>
+                        <Form.Input
+                            error={!!componentState.error}
+                            size="huge"
+                            value={componentState.address}
+                            placeholder="0x0"
+                            action={{
+                                position: "right",
+                                content: "Submit",
+                                color: "blue",
+                                size: "huge",
+                                onClick: submitFaucetRequest,
+                                loading: componentState.loading
+                            }}
+                            onChange={e => updateComponentState({ address: e.target.value })}
+                        />
+                    </Form>
+
+                    {componentState.loading && <Header as="h5" color="grey" content="Please wait funding in progress ..." />}
+
+                    <ResultMessage />
+
+                </Container>
+            </Grid.Column>
+        </Grid >
+    );
 }
 
 export default App;
